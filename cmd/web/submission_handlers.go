759c175ef1be9d504a3b6d4748a63d273159094e
Kai Blin <kblin@biosustain.dtu.dk>
2019-07-23 13:03:41 +0200

    web: Move submission handlers to a separate file
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

---
+++ cmd/web/submission_handlers.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"secondarymetabolites.org/mibig-api/pkg/models"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func (app *application) submit(c *gin.Context) {
+	var req models.AccessionRequest
+	c.BindJSON(&req)
+
+	if err := app.Mail.Send(req.Email, generateRequestMailBody(&req, app.Mail.Config().Recipient)); err != nil {
+		app.serverError(c, err)
+		return
+	}
+
+	c.String(http.StatusAccepted, "")
+}
+
+func generateRequestMailBody(req *models.AccessionRequest, recipient string) []byte {
+	compound := strings.Join(req.Compounds, ", ")
+	var loci_parts []string
+	for _, locus := range req.Loci {
+		loci_parts = append(loci_parts, fmt.Sprintf("  %s (%d - %d)", locus.GenBankAccession, locus.Start, locus.End))
+	}
+	loci := strings.Join(loci_parts, "\n")
+
+	return []byte(fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: MIBiG update / request\r\n\r\nName: %s\nEmail: %s\nCompound: %s\nLoci:\n%s",
+		req.Email, recipient, req.Name, req.Email, compound, loci))
+}
+
+func (app *application) LegacyStoreSubmission(c *gin.Context) {
+	mibigJson := c.PostForm("json")
+	versionString := c.PostForm("version")
+
+	if mibigJson == "" {
+		app.logger.Infow("no JSON provided")
+		c.JSON(400, gin.H{"error": true, "message": "json not provided"})
+		return
+	}
+
+	if !json.Valid([]byte(mibigJson)) {
+		app.logger.Infow("invalid json string", "json", mibigJson)
+		c.JSON(400, gin.H{"error": true, "message": "Invalid JSON input"})
+		return
+	}
+
+	if versionString == "" {
+		app.logger.Infow("no version provided")
+		c.JSON(400, gin.H{"error": true, "message": "Version parameter not provided. Need a version parameter greater than 0"})
+		return
+	}
+
+	version, err := strconv.ParseInt(versionString, 10, 32)
+	if err != nil {
+		app.logger.Infow("invalid version string", "version_string", versionString)
+		c.JSON(400, gin.H{"error": true, "message": "Version parameter not a valid number"})
+		return
+	}
+
+	if version <= 0 {
+		app.logger.Infow("version too small", "version", version)
+		c.JSON(400, gin.H{"error": true, "message": "Need a version parameter greater than 0"})
+		return
+	}
+
+	submission := models.LegacySubmission{
+		Submitted: time.Now().UTC(),
+		Modified:  time.Now().UTC(),
+		Raw:       mibigJson,
+		Version:   int(version),
+	}
+
+	if err := app.LegacyModel.CreateSubmission(&submission); err != nil {
+		app.serverError(c, err)
+		return
+	}
+
+	c.Redirect(http.StatusSeeOther, "/static/genes_form.html")
+}
+
+func (app *application) LegacyStoreBgcDetailSubmission(c *gin.Context) {
+	data := c.PostForm("data")
+	target := c.PostForm("target")
+	versionString := c.DefaultPostForm("version", "1")
+	bgc_id := c.DefaultPostForm("bgc_id", "BGC00000")
+
+	if data == "" {
+		app.logger.Infow("no JSON provided")
+		c.JSON(400, gin.H{"error": true, "message": "json not provided"})
+		return
+	}
+
+	if !json.Valid([]byte(data)) {
+		app.logger.Infow("invalid json string", "json", data)
+		c.JSON(400, gin.H{"error": true, "message": "Invalid JSON input"})
+		return
+	}
+
+	if target == "" {
+		app.logger.Infow("no target provided")
+		c.JSON(400, gin.H{"error": true, "message": "target not provided"})
+		return
+	}
+
+	if versionString == "" {
+		app.logger.Infow("no version provided")
+		c.JSON(400, gin.H{"error": true, "message": "Version parameter not provided. Need a version parameter greater than 0"})
+		return
+	}
+
+	version, err := strconv.ParseInt(versionString, 10, 32)
+	if err != nil {
+		app.logger.Infow("invalid version string", "version_string", versionString)
+		c.JSON(400, gin.H{"error": true, "message": "Version parameter not a valid number"})
+		return
+	}
+
+	if version <= 0 {
+		app.logger.Infow("version too small", "version", version)
+		c.JSON(400, gin.H{"error": true, "message": "Need a version parameter greater than 0"})
+		return
+	}
+
+	if target == "gene_info" {
+		submission := models.LegacyGeneSubmission{
+			BgcId:     bgc_id,
+			Submitted: time.Now().UTC(),
+			Modified:  time.Now().UTC(),
+			Raw:       data,
+			Version:   int(version),
+		}
+
+		err = app.LegacyModel.CreateGeneSubmission(&submission)
+	} else if target == "nrps_info" {
+		submission := models.LegacyNrpsSubmission{
+			BgcId:     bgc_id,
+			Submitted: time.Now().UTC(),
+			Modified:  time.Now().UTC(),
+			Raw:       data,
+			Version:   int(version),
+		}
+
+		err = app.LegacyModel.CreateNrpsSubmission(&submission)
+	} else {
+		app.logger.Infow("Invalid details submission target", "target", target)
+		c.JSON(400, gin.H{"error": true, "message": "target parameter not matching. Must be one of 'gene_info' or 'nrps_info'"})
+		return
+	}
+
+	if err != nil {
+		app.serverError(c, err)
+		return
+	}
+
+	c.AbortWithStatus(204)
+}
---
+++ cmd/web/submission_handlers_test.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/andreyvit/diff"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"secondarymetabolites.org/mibig-api/pkg/models"
+	"strings"
+	"testing"
+)
+
+func TestSubmit(t *testing.T) {
+	_, ts, mail_rec := newTestApp()
+	defer ts.Close()
+
+	expected_body := strings.Join([]string{
+		"From: alice@example.com\r",
+		"To: alice@example.com\r",
+		"Subject: MIBiG update / request\r",
+		"\r",
+		"Name: Alice",
+		"Email: alice@example.com",
+		"Compound: testomycin",
+		"Loci:",
+		"  ABC12345 (23 - 42)",
+	}, "\n")
+
+	req := models.AccessionRequest{
+		Name:      "Alice",
+		Email:     "alice@example.com",
+		Compounds: []string{"testomycin"},
+		Loci: []models.AccessionRequestLocus{
+			models.AccessionRequestLocus{
+				Start:            23,
+				End:              42,
+				GenBankAccession: "ABC12345",
+			},
+		},
+	}
+
+	raw_req, err := json.Marshal(&req)
+	req_body := bytes.NewReader(raw_req)
+
+	response, err := ts.Client().Post(ts.URL+"/api/v1/submit", "application/json", req_body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.StatusCode != http.StatusAccepted {
+		t.Errorf("Expected %d, got %d (%s)", http.StatusAccepted, response.StatusCode, string(body))
+	}
+
+	if actual, expected := strings.TrimSpace(string(mail_rec.msg)), strings.TrimSpace(expected_body); actual != expected {
+		t.Errorf("Unexpected email body:\n%v", diff.LineDiff(expected, actual))
+	}
+
+}
+
+func TestLegacySubmission(t *testing.T) {
+	_, ts, _ := newTestApp()
+	defer ts.Close()
+
+	form := url.Values{}
+	form.Set("json", `{"foo": "bar"}`)
+	form.Set("version", "1")
+
+	response, err := ts.Client().Post(ts.URL+"/api/v1/bgc-registration", "application/x-www-form-urlencoded", strings.NewReader(form.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		t.Errorf("Expected %d, got %d (%s)", http.StatusOK, response.StatusCode, string(body))
+	}
+}
+
+func TestLegacyGeneSubmission(t *testing.T) {
+	_, ts, _ := newTestApp()
+	defer ts.Close()
+
+	form := url.Values{}
+	form.Set("data", `{"foo": "bar"}`)
+	form.Set("version", "1")
+	form.Set("bgc_id", "BGC1234567")
+	form.Set("target", "gene_info")
+
+	response, err := ts.Client().Post(ts.URL+"/api/v1/bgc-detail-registration", "application/x-www-form-urlencoded", strings.NewReader(form.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.StatusCode != http.StatusNoContent {
+		t.Errorf("Expected %d, got %d (%s)", http.StatusNoContent, response.StatusCode, string(body))
+	}
+}
+
+func TestLegacyNrpsSubmission(t *testing.T) {
+	_, ts, _ := newTestApp()
+	defer ts.Close()
+
+	form := url.Values{}
+	form.Set("data", `{"foo": "bar"}`)
+	form.Set("version", "1")
+	form.Set("bgc_id", "BGC1234567")
+	form.Set("target", "nrps_info")
+
+	response, err := ts.Client().Post(ts.URL+"/api/v1/bgc-detail-registration", "application/x-www-form-urlencoded", strings.NewReader(form.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.StatusCode != http.StatusNoContent {
+		t.Errorf("Expected %d, got %d (%s)", http.StatusNoContent, response.StatusCode, string(body))
+	}
+}
